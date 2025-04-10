e83e3c9aaf0306638a24fa84521f1f07a27e4dce
SoumitraKode <soumitrakode22@gmail.com>
2025-04-10 19:36:59 +0530

    Edited the RazorPayId

--- server/config/razorpay.js
+++ server/config/razorpay.js
@@ -1,7 +1,7 @@
 const Razorpay = require("razorpay");
-
+const dotenv = require("dotenv") ;
 
 exports.instance = new Razorpay({
-    key_id: process.env.RAZORPAY_KEY,
-    key_secret: process.env.RAZORPAY_SECRET,
+    key_id: 'rzp_test_Td0LKIEJby5SR8',
+    key_secret: 'GhFaRjtbaHzM26Hg4NVwiX4e',
 });
--- src/services/operations/studentFeaturesAPI.js
+++ src/services/operations/studentFeaturesAPI.js
@@ -67,7 +67,7 @@ export async function BuyCourse(
 
     // Opening the Razorpay SDK
     const options = {
-      key: process.env.RAZORPAY_KEY,
+      key: 'rzp_test_Td0LKIEJby5SR8',
       currency: orderResponse.data.data.currency,
       amount: `${orderResponse.data.data.amount}`,
       order_id: orderResponse.data.data.id,
